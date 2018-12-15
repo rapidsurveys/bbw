84df0d9f99267c0103e8487a5c622f2df5ea16b5
Ernest Guevarra <ernest@validmeasures.org>
2018-12-15 22:13:06 +0000

    create inst/

---
+++ inst/createHexSticker.R
@@ -0,0 +1,7 @@
+library(hexSticker)
+
+sticker("inst/figures/roulette.png",
+        s_x = 1, s_y = 0.8, s_width = 0.7,
+        package = "bbw", p_color = "#E3BB00", p_y = 1.6,
+        h_size = 1, h_color = "#E3BB00", h_fill = NA,
+        filename = "man/figures/bbw.png")
